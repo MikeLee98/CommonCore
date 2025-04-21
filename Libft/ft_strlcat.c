c156d1f96c6395c32c0936857ca21a1631ed6667
Mario Rodrigues <marioro2@c2r4s13.42lisboa.com>
2025-04-21 12:50:41 +0100

    patch

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: marioro2 <marioro2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 14:54:05 by marioro2          #+#    #+#             */
-/*   Updated: 2025/04/21 12:44:23 by marioro2         ###   ########.fr       */
+/*   Updated: 2025/04/21 12:50:35 by marioro2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,52 +45,53 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	return (dst_len + src_len);
 }
 
-// int main() 
-// {
-//     char buffer0[4] = "dst\0";
-//     size_t result0 = ft_strlcat(buffer0, "src", 0);
-// 	printf("Resultado: %s\n", buffer0);
-//     printf("Comprimento total pretendido: %zu\n", result0);
+#include <stdio.h>
+
+int main() 
+{
+    char buffer0[4] = "dst\0";
+    size_t result0 = ft_strlcat(buffer0, "src", 0);
+	printf("Resultado: %s\n", buffer0);
+    printf("Comprimento total pretendido: %zu\n", result0);
 	
-// 	char buffer1[5] = "dst";
-// 	size_t result1 = ft_strlcat(buffer1, "src", 1);
-// 	printf("Resultado: %s\n", buffer1);
-//     printf("Comprimento total pretendido: %zu\n", result1);
+	char buffer1[5] = "dst";
+	size_t result1 = ft_strlcat(buffer1, "src", 1);
+	printf("Resultado: %s\n", buffer1);
+    printf("Comprimento total pretendido: %zu\n", result1);
 	
-// 	char buffer2[6] = "dst";
-// 	size_t result2 = ft_strlcat(buffer2, "src", 2);
-// 	printf("Resultado: %s\n", buffer2);
-//     printf("Comprimento total pretendido: %zu\n", result2);
+	char buffer2[6] = "dst";
+	size_t result2 = ft_strlcat(buffer2, "src", 2);
+	printf("Resultado: %s\n", buffer2);
+    printf("Comprimento total pretendido: %zu\n", result2);
 
-// 	char buffer3[7] = "dst";
-// 	size_t result3 = ft_strlcat(buffer3, "src", 3);
-// 	printf("Resultado: %s\n", buffer3);
-//     printf("Comprimento total pretendido: %zu\n", result3);
+	char buffer3[7] = "dst";
+	size_t result3 = ft_strlcat(buffer3, "src", 3);
+	printf("Resultado: %s\n", buffer3);
+    printf("Comprimento total pretendido: %zu\n", result3);
 	
-// 	char buffer4[8] = "dst";
-// 	size_t result4 = ft_strlcat(buffer4, "src", 4);
-// 	printf("Resultado: %s\n", buffer4);
-//     printf("Comprimento total pretendido: %zu\n", result4);
+	char buffer4[8] = "dst";
+	size_t result4 = ft_strlcat(buffer4, "src", 4);
+	printf("Resultado: %s\n", buffer4);
+    printf("Comprimento total pretendido: %zu\n", result4);
 
-// 	char buffer5[9] = "dst";
-// 	size_t result5 = ft_strlcat(buffer5, "src", 5);
-// 	printf("Resultado: %s\n", buffer5);
-//     printf("Comprimento total pretendido: %zu\n", result5);
+	char buffer5[9] = "dst";
+	size_t result5 = ft_strlcat(buffer5, "src", 5);
+	printf("Resultado: %s\n", buffer5);
+    printf("Comprimento total pretendido: %zu\n", result5);
 
-// 	char buffer6[10] = "dst";
-// 	size_t result6 = ft_strlcat(buffer6, "src", 6);
-// 	printf("Resultado: %s\n", buffer6);
-//     printf("Comprimento total pretendido: %zu\n", result6);
+	char buffer6[10] = "dst";
+	size_t result6 = ft_strlcat(buffer6, "src", 6);
+	printf("Resultado: %s\n", buffer6);
+    printf("Comprimento total pretendido: %zu\n", result6);
 	
-// 	char buffer7[11] = "dst";
-// 	size_t result7 = ft_strlcat(buffer7, "src", 7);
-// 	printf("Resultado: %s\n", buffer7);
-//     printf("Comprimento total pretendido: %zu\n", result7);
+	char buffer7[11] = "dst";
+	size_t result7 = ft_strlcat(buffer6, "src", 7);
+	printf("Resultado: %s\n", buffer7);
+    printf("Comprimento total pretendido: %zu\n", result7);
 
-// 	char buffer8[12] = "dst";
-// 	size_t result8 = ft_strlcat(buffer8, "src", 8);
-// 	printf("Resultado: %s\n", buffer8);
-//     printf("Comprimento total pretendido: %zu\n", result8);
+	char buffer8[12] = "dst";
+	size_t result8 = ft_strlcat(buffer6, "src", 8);
+	printf("Resultado: %s\n", buffer8);
+    printf("Comprimento total pretendido: %zu\n", result8);
 
-//     return 0;
-// }
+}
