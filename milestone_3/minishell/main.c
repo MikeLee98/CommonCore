dca2f71d3559380ecfb4063c9103d119afc7791b
Mario Rodrigues <marioro2@c1r11s6.42lisboa.com>
2025-11-18 19:12:34 +0000

    minishell update

--- milestone_3/minishell/main.c
+++ milestone_3/minishell/main.c
@@ -6,7 +6,7 @@
 /*   By: marioro2 <marioro2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 16:44:30 by marioro2          #+#    #+#             */
-/*   Updated: 2025/11/18 17:56:00 by marioro2         ###   ########.fr       */
+/*   Updated: 2025/11/18 18:59:20 by marioro2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@
 
 int main(void)
 {
-	char *line;
+	char *input;
 	
 	while(1)
 	{
-		line = readline("minishell$>");
-		parser(line);
-		free(line);
+		input = readline("minishell$>");
+		parser(input);
+		free(input);
 	}
 }
